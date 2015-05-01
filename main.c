c097e9cd13c4be3e10bfa57053d79df8a6b2cd2e
Jiloko <gwenael.larmet@gmail.com>
2015-05-01 13:13:48 +0200

    Affichage des lignes du polygone de base

--- main.c
+++ main.c
@@ -12,8 +12,8 @@ main.c
 #include "Polygon.h"
 #include "Mesh.h"
 
-Polygon P ;
-Mesh M ;
+Polygon P;
+Mesh M;
 
 int draw = 0;
 Vector p_aim;
@@ -52,22 +52,33 @@ void drawRepere()
 //------------------------------------------------------------
 
 
-void dessinerVecteur( Vector p1){
+void dessinerVecteur(Vector p1)
+{
 	glBegin(GL_LINES);
 	glVertex3d(0,0,0);
 	glVertex3d(p1.x,p1.y,p1.z);
-	glEnd() ;
+	glEnd();
 }
 
-void dessinerPoint( Vector p ){
+void dessinerPoint(Vector p)
+{
 	glPointSize(10.0);
 	glBegin(GL_POINTS);
-	glVertex3f( p.x , p.y , p.z ) ;
+	glVertex3f(p.x ,p.y ,p.z);
+	glEnd();
+}
+
+void dessinerLigne(Vector p1, Vector p2)
+{
+	glBegin(GL_LINES);
+	glVertex3d(p1.x,p1.y,p1.z);
+	glVertex3d(p2.x,p2.y,p2.z);
 	glEnd();
 }
 
 void display()
 {
+	int i;
 	glEnable(GL_DEPTH_TEST);
 	glDepthMask(GL_TRUE);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -75,14 +86,18 @@ void display()
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
-	int i;
 	for(i=0; i < P._nb_vertices; i++)
+	{
 		dessinerPoint(P._vertices[i]);
+		if (i+1 < P._nb_vertices)
+			dessinerLigne(P._vertices[i], P._vertices[i+1]);
+	}
 
 	if( !stop_input )
 		glOrtho(-1,1,-1,1,-1,1);
-	else {
-		gluPerspective( 30, (float)width/height, 1, 100);
+	else 
+	{
+		gluPerspective(30, (float)width/height, 1, 100);
 		gluLookAt(2,2,4,0,0,0,0,1,0);
 		glTranslatef(p_aim.x,p_aim.y,p_aim.z);
 		glRotatef(theta,1,0,0);
@@ -92,18 +107,18 @@ void display()
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	if( stop_input )
+	if(stop_input)
+		M_draw(&M);
+	else
 	{
-		M_draw(&M) ;
-	}
-	else{
-		if( P._is_convex )
+		if(P._is_convex)
 			glColor3d(0,0,128);
-		else glColor3d(128,0,0);
-		P_draw( &P );
+		else 
+			glColor3d(128,0,0);
+		P_draw(&P);
 		glColor3d(128,128,128);
-		dessinerPoint( P_center( &P ) ) ;
-		dessinerVecteur( P_normal( &P) );
+		dessinerPoint(P_center(&P));
+		//dessinerVecteur(P_normal(&P));
 	}
 
 	drawRepere();
@@ -131,22 +146,22 @@ void initShade()
 
 //------------------------------------------------------------
 
-void mode_dessin(){
-	switch(draw){
-		case 0 :
+void mode_dessin()
+{
+	switch(draw)
+	{
+		case 0:
 			draw = 1;
 			M._is_filled = 0;
 			glDisable(GL_LIGHTING);
 			glEnable(GL_DEPTH_TEST);
 			break;
-
-		case 1 :
+		case 1:
 			draw = 2;
 			M._is_filled = 1;
 			initShade();
 			break;
-
-		case 2 :
+		case 2:
 			draw = 0;
 			M._is_filled = 1;
 			glDisable(GL_LIGHTING);
@@ -159,7 +174,8 @@ void keyboard(unsigned char keycode, int x, int y)
 {
 	printf("touche : (code ascii %d)\n",keycode);
 
-	if(keycode=='d') // changer l'affichage
+	/* touche SPACE */
+	if(keycode==32)
 		mode_dessin() ;
 
 	/* touche ECHAP */
@@ -187,7 +203,9 @@ void special(int keycode, int x, int y)
 		case GLUT_KEY_RIGHT: 
 			phi+= 10;
 			break;
-		default : fprintf(stderr,"function special : unknown keycode %d\n",keycode); break;
+		default : 
+			fprintf(stderr,"function special : unknown keycode %d\n",keycode);
+		 	break;
 	}
 	glutPostRedisplay();
 }
@@ -197,35 +215,32 @@ void special(int keycode, int x, int y)
 void mouse(int button, int state, int x, int y)
 {
 	float x_coord = ((float)x - (float)width/2)/(float)(width/2);
-	float y_coord = ((float)height/2 - (float)y)/(float)(height/2) ;
-	switch(button){
+	float y_coord = ((float)height/2 - (float)y)/(float)(height/2);
+	switch(button)
+	{
 		case GLUT_LEFT_BUTTON :
-			if(state==GLUT_DOWN){
-				Vector new_point = V_new( x_coord , y_coord , 0 ) ;
-				if( !stop_input && !P_isIntersectedByARay(&P , new_point , P._vertices[P._nb_vertices-1])){
-					P_addVertex( &P , new_point ) ;	
-					P_isConvex( &P ) ;
+			if(state==GLUT_DOWN)
+			{
+				Vector new_point = V_new(x_coord, y_coord, 0);
+				if(!stop_input && !P_isIntersectedByARay(&P, new_point, P._vertices[P._nb_vertices-1]))
+				{
+					P_addVertex(&P, new_point);	
+					P_isConvex(&P);
 				}
 			}
 			glutPostRedisplay();
 			break;
 
-		case GLUT_MIDDLE_BUTTON :
-			if(state==GLUT_DOWN)
-				stop_input = 1 ;
-			glutPostRedisplay();
-			break;
-
 		case GLUT_RIGHT_BUTTON :
-			if(state==GLUT_DOWN){
-				P._is_closed = 1 ;
-				M_init( &M ) ;
-				M_perlinExtrude( &M , &P , 128 );
-				stop_input = 1 ;
-				glutPostRedisplay();
-				break;
+			if(state==GLUT_DOWN)
+			{
+				P._is_closed = 1;
+				M_init(&M);
+				M_perlinExtrude(&M, &P, 128);
+				stop_input = 1;
 			}
 			glutPostRedisplay();
+			break;
 	}
 }
 
@@ -234,7 +249,7 @@ void mouse(int button, int state, int x, int y)
 /*************************************************************************/
 int main(int argc, char *argv[])
 {	
-	P_init( &P ) ;
+	P_init(&P);
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
